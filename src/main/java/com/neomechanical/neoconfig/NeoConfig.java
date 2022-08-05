5a59e4860eeef23cfbad2f44ecebbeadcaa13168
KyTDK <71435411+KyTDK@users.noreply.github.com>
2022-08-05 20:02:55 +1000

    1.0.1 - Added metrics

--- src/main/java/com/neomechanical/neoconfig/NeoConfig.java
+++ src/main/java/com/neomechanical/neoconfig/NeoConfig.java
@@ -4,6 +4,8 @@ import com.neomechanical.neoconfig.commands.RegisterCommands;
 import com.neomechanical.neoutils.inventory.InventoryUtil;
 import lombok.NonNull;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
+import org.bstats.bukkit.Metrics;
+import org.bstats.charts.SimplePie;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NeoConfig extends JavaPlugin {
@@ -21,6 +23,7 @@ public final class NeoConfig extends JavaPlugin {
         }
         return adventure;
     }
+    private Metrics metrics;
     @Override
     public void onEnable() {
         setInstance(this);
@@ -28,10 +31,24 @@ public final class NeoConfig extends JavaPlugin {
         adventure = BukkitAudiences.create(this);
         InventoryUtil.init(NeoConfig.getInstance());
         RegisterCommands.register();
+        setupBStats();
     }
 
+    public void setupBStats() {
+        int pluginId = 16032;
+        metrics = new Metrics(this, pluginId);
+    }
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+    /**
+     * Returns an instance of the bStats Metrics object
+     *
+     * @return bStats Metrics object
+     */
+    @SuppressWarnings("unused")
+    public Metrics getMetrics() {
+        return metrics;
     }
 }
