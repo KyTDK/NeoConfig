951d0f58e72611c4dc8f8edf01faf07e73fe8aa3
KyTDK <71435411+KyTDK@users.noreply.github.com>
2022-11-29 16:10:05 +1100

    ConversationEditor.java

--- api/src/main/java/com/neomechanical/neoconfig/menu/actions/ConversationEditor.java
+++ api/src/main/java/com/neomechanical/neoconfig/menu/actions/ConversationEditor.java
@@ -1,7 +1,6 @@
 package com.neomechanical.neoconfig.menu.actions;
 
-import org.bukkit.conversations.Conversation;
-import org.bukkit.conversations.Prompt;
+import org.bukkit.conversations.*;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -12,6 +11,23 @@ public class ConversationEditor {
         this.plugin = plugin;
     }
     public void main(Player player) {
-        Conversation conversation = new Conversation(plugin, player, new Prompt());
+        Prompt prompt = new StringPrompt() {
+            @Override
+            public String getPromptText(ConversationContext context) {
+                return "Long live the king!";
+            }
+
+            @Override
+            public Prompt acceptInput(ConversationContext context, String input) {
+                // Here we connect the next prompt
+                return Prompt.END_OF_CONVERSATION;
+            }
+        };
+        ;
+        ConversationFactory factory = new ConversationFactory(plugin); // We need our plugin reference here
+        factory.withFirstPrompt(prompt);
+        Conversation conversation = factory.buildConversation(player);
+        conversation.begin();
+
     }
 }
